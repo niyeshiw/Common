83bbae8b135697e825118d0c20e675f4615861ec
niyeshiw <745310478@qq.com>
2017-09-15 21:54:54 +0800

    新增对ConnectionString和AppSetting的支持

---
+++ Common/ConfigHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Common
+{
+    public class ConfigHelper
+    {
+        public static string ConnectionString(string name)
+        {
+            if (string.IsNullOrEmpty(name)) throw new ArgumentNullException();
+            return System.Configuration.ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+
+        public static string AppSetting(string name)
+        {
+            if (string.IsNullOrEmpty(name)) throw new ArgumentNullException();
+            return System.Configuration.ConfigurationManager.AppSettings[name].ToString();
+        }
+    }
+}
